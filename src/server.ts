6ebdff83589c6d7a08723db848aee91537138004
William Almeida <willliam.dev@gmail.com>
2022-11-01 19:32:13 -0300

    :art: returning pools from db
    return pools from db with prisma client

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
 import Fastify from "fastify";
 
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient({
+  log: ["query"]
+})
+
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   })
 
-  fastify.get("/pools/count", () => {
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count()
+
     return {
-      count: 34
+      count
     }
   })
 
