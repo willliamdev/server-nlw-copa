4187b97de2dfa50d427dcc00b289c3a33c046045
William Almeida <willliam.dev@gmail.com>
2022-11-01 18:53:10 -0300

    :tada: first commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from "fastify";
+
+
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+
+  await fastify.listen({ port: 3333 })
+}
+
+
+bootstrap()
