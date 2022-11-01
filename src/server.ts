01b52e755e786a75f4a99491965fb88057571a27
William Almeida <willliam.dev@gmail.com>
2022-11-01 19:09:52 -0300

    :heavy_plus_sign: add prisma and prisma-client

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import Fastify from "fastify";
 
-
-
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   })
 
+  fastify.get("/pools/count", () => {
+    return {
+      count: 34
+    }
+  })
 
   await fastify.listen({ port: 3333 })
 }
-
 
 bootstrap()
