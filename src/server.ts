ad714541d43adbaabfd6ea9f8997806ba8068cc2
William Almeida <willliam.dev@gmail.com>
2022-11-02 23:17:49 -0300

    :sparkles: create post route "/pools"

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 
+import ShortUniqueId from "short-unique-id";
+import { z } from "zod";
+
+
 const prisma = new PrismaClient({
   log: ["query"]
 });
@@ -23,6 +27,27 @@ async function bootstrap() {
     };
   });
 
+  fastify.post("/pools", async (request, response) => {
+    const createPoolBody = z.object({
+      title: z.string()
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title: title,
+        code
+      }
+    });
+
+
+
+    return response.status(201).send({ code });
+  });
   await fastify.listen({ port: 3333,/* host: "0.0.0.0"*/ });
 }
 
