593ec640c435a9eb11790343eaf30c1007dc5044
William Almeida <willliam.dev@gmail.com>
2022-11-02 23:47:32 -0300

    :art: add get routes

--- src/server.ts
+++ src/server.ts
@@ -22,18 +22,14 @@ async function bootstrap() {
   fastify.get("/pools/count", async () => {
     const count = await prisma.pool.count();
 
-    return {
-      count
-    };
+    return { count };
   });
 
   fastify.post("/pools", async (request, response) => {
     const createPoolBody = z.object({
       title: z.string()
     });
-
     const { title } = createPoolBody.parse(request.body);
-
     const generate = new ShortUniqueId({ length: 6 });
     const code = String(generate()).toUpperCase();
 
@@ -44,10 +40,21 @@ async function bootstrap() {
       }
     });
 
-
-
     return response.status(201).send({ code });
   });
+
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
   await fastify.listen({ port: 3333,/* host: "0.0.0.0"*/ });
 }
 
